461bbd2267e295fe551eb01ea25db009adf5c912
ImAtLeast12 <imatleast12@gmail.com>
2017-03-15 17:16:52 -0400

    I fixed swiped the temps around so it follows the same format
    I am going to change it so that it works as a single array for the cube
    hopefully this will make it easier so I can split values easier.

--- RubiksCube.cpp
+++ RubiksCube.cpp
@@ -20,6 +20,8 @@ enum Faces(){
 char FACE_NAME [6][20] = {"LEFT","BACK","UP","FRONT","RIGHT","DOWN"};
 
 enum FACES{LEFT,BACK,UP,FRONT,RIGHT,DOWN};
+enum COLORS{GREEN, ORANGE, WHITE, RED, BLUE, YELLOW};
+void spinX(int (&cube)[NUM_FACES][NUM_STICKERS]);
 
 void print(int (&cube)[NUM_FACES][NUM_STICKERS]); //This prints out the cube for me in the order of Front,Back,Up,Down,Left,Right ~ it dosn't really matter what it is mapped to 
 void initilize(int (&cube)[NUM_FACES][NUM_STICKERS]);
@@ -33,49 +35,13 @@ void moveDOWN(int (&cube)[NUM_FACES][NUM_STICKERS]);
 void moveFRONT(int (&cube)[NUM_FACES][NUM_STICKERS]);
 void moveBACK(int (&cube)[NUM_FACES][NUM_STICKERS]);
 int main(){
+
 	int cube [NUM_FACES][NUM_STICKERS];  //MY GOAL IS TO HAVE THIS BE AS FLEXIBLE AS POSSIBLE
 	initilize(cube);
 	print(cube); 
 
-	char steps[] = "U' R2 B R' L' F D' F' ";
-	moveUP(cube);moveUP(cube);moveUP(cube);
-	moveRIGHT(cube);moveRIGHT(cube);
-	moveBACK(cube);
-	moveRIGHT(cube);moveRIGHT(cube);moveRIGHT(cube);
-	moveLEFT(cube); moveLEFT(cube); moveLEFT(cube);
-	moveFRONT(cube);
-	moveDOWN(cube);moveDOWN(cube);moveDOWN(cube);
-	moveFRONT(cube);moveFRONT(cube);moveFRONT(cube);
-	print(cube);
-	moveFRONT(cube);
-	moveDOWN(cube);
-	moveFRONT(cube);moveFRONT(cube);moveFRONT(cube);
-	moveLEFT(cube);
-	moveRIGHT(cube);
-	moveBACK(cube);moveBACK(cube);moveBACK(cube);
-	moveRIGHT(cube);moveRIGHT(cube);
-	moveUP(cube);
-	print(cube);
-
-	//PUT TOP UNDER HERE
-	
-	//This is where I am going to test a single turn of the cube
-	//I am going to first predict what numbers I should see before I do this then see if it is doing what I want as expected 
-	//I will then have to repeat this process for all ways you can turn the cube
+	spinX(cube);
 }
-
-
-
-/*
-Something to keep in mind whenever I do a turn of a face it is basically going to be the same movement only on another side of the cube
-Another thing to keep in mind when ever I do a turn of a face it will only affect 12 stickers, on 4 faces; 3 stickers on each side. 
-In otherwords 2 sides will be uneffected, and the sides that are affected will be uniform 
-The only thing is trying to visualise what I need to change and what the output should look like in terms of numbers.
-*/
-
-
-//This project might be even easier if I think the cube as being a large array of size 54 (meaning 6*9 length)
-//This might make it simpler when trying to swap all of the places around
 
 void moveBACK(int (&cube)[NUM_FACES][NUM_STICKERS]){
 	int temp = cube[UP][0];
@@ -166,9 +132,9 @@ void moveDOWN(int (&cube)[NUM_FACES][NUM_STICKERS]){
 }
 
 void moveUP(int (&cube)[NUM_FACES][NUM_STICKERS]){
-	int temp = cube[LEFT][1];
-	int temp0 = cube[LEFT][2];
-	int temp1 = cube[LEFT][0];
+	int temp = cube[LEFT][0];
+	int temp0 = cube[LEFT][1];
+	int temp1 = cube[LEFT][2];
 	cube[LEFT][0] = cube[FRONT][0];
 	cube[LEFT][1] = cube[FRONT][1];
 	cube[LEFT][2] = cube[FRONT][2];
@@ -178,9 +144,9 @@ void moveUP(int (&cube)[NUM_FACES][NUM_STICKERS]){
 	cube[RIGHT][0] = cube[BACK][0];
 	cube[RIGHT][1] = cube[BACK][1];
 	cube[RIGHT][2] = cube[BACK][2];
-	cube[BACK][0] = temp1;
-	cube[BACK][1] = temp;
-	cube[BACK][2] = temp0;
+	cube[BACK][0] = temp;
+	cube[BACK][1] = temp0;
+	cube[BACK][2] = temp1;
 	
 	temp = cube[UP][0];//I will also have to switch the roatation of the face it's self
 	cube[UP][0] = cube[UP][6];
@@ -195,9 +161,9 @@ void moveUP(int (&cube)[NUM_FACES][NUM_STICKERS]){
 }
 
 void moveRIGHT(int (&cube)[NUM_FACES][NUM_STICKERS]){
-	int temp = cube[UP][2];
+	int temp = cube[UP][8];
 	int temp0 = cube[UP][5];
-	int temp1 = cube[UP][8];
+	int temp1 = cube[UP][2];
 	cube[UP][2] = cube[FRONT][2];
 	cube[UP][5] = cube[FRONT][5];
 	cube[UP][8] = cube[FRONT][8];
@@ -207,9 +173,9 @@ void moveRIGHT(int (&cube)[NUM_FACES][NUM_STICKERS]){
 	cube[DOWN][0] = cube[BACK][6];
 	cube[DOWN][1] = cube[BACK][3];
 	cube[DOWN][2] = cube[BACK][0];
-	cube[BACK][0] = temp1;
+	cube[BACK][0] = temp;
 	cube[BACK][3] = temp0;
-	cube[BACK][6] = temp;
+	cube[BACK][6] = temp1;
 
 	temp = cube[RIGHT][0]; //I will also have to switch the roatation of the face it's self
 	cube[RIGHT][0] = cube[RIGHT][6];
@@ -236,9 +202,10 @@ void moveLEFT(int (&cube)[NUM_FACES][NUM_STICKERS]){
 	cube[DOWN][8] = cube[FRONT][6];
 	cube[DOWN][7] = cube[FRONT][3];
 	cube[DOWN][6] = cube[FRONT][0];
-	cube[FRONT][6] = temp1;
-	cube[FRONT][3] = temp0;
 	cube[FRONT][0] = temp;
+	cube[FRONT][3] = temp0;
+	cube[FRONT][6] = temp1;
+	
 	
 	temp = cube[LEFT][0]; //I will also have to switch the roatation of the face it's self
 	cube[LEFT][0] = cube[LEFT][6];
